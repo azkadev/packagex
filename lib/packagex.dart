99e4fd10105e6993b453d74d33f574f8d2ee6b0f
￴ <>
2022-12-05 00:55:18 +0700

    update

--- lib/packagex.dart
+++ lib/packagex.dart
@@ -1,7 +1,8 @@
-// ignore_for_file: unused_local_variable, unnecessary_brace_in_string_interps, unused_element
+// ignore_for_file: unused_local_variable, unnecessary_brace_in_string_interps, unused_element, non_constant_identifier_names, empty_catches
 
 library packagex;
 
+import 'dart:convert';
 import 'dart:io';
 import 'package:path/path.dart' as p;
 
@@ -14,7 +15,7 @@ class PackageBuild {
     required String name,
     String maintaner = "-",
     required String package,
-    bool isForce = false,
+    bool isForce = true,
     double version = 0.0,
     String architecture = "amd64",
     String essential = "no",
@@ -31,9 +32,18 @@ Essential: no
 Description: "${description}"
 Homepage: "${homepage}"
 """;
+    String app_desktop_linux = """
+[Desktop Entry]
+Type=Application
+Version=0.0.0
+Name=${package}
+GenericName=General Application
+Exec=${package} %U
+Categories=Music;Media;
+Keywords=Hello;World;Test;Application;
+StartupNotify=true
+""";
     Directory directory = Directory(p.join(Directory.current.path, name));
-    await directory.create(recursive: true);
-
     Future<void> createFolders({
       required Directory directory,
       required List<List<String>> folders,
@@ -56,6 +66,9 @@ Homepage: "${homepage}"
         try {
           await File(p.join(directory.path, "DEBIAN", "control")).writeAsString(scripts);
         } catch (e) {}
+        try {
+          await File(p.join(directory.path, "usr", "local", "share", "applications", "${package}.desktop")).writeAsString(app_desktop_linux);
+        } catch (e) {}
       }
       return;
     }
@@ -70,7 +83,9 @@ Homepage: "${homepage}"
     );
     await stdout.addStream(shell.stdout);
     // await stderr.addStream(shell.stderr);
-
+    if (!directory.existsSync()) {
+      await directory.create(recursive: true);
+    }
     await createFolders(
       directory: Directory(p.join(directory.path, "android", "packaging")),
       folders: [],
@@ -88,6 +103,7 @@ Homepage: "${homepage}"
         ["usr", "local"],
         ["usr", "local", "bin"],
         ["usr", "local", "lib"],
+        ["usr", "local", "share", "applications"],
         ["usr", "local", "share", package],
       ],
     );
@@ -107,11 +123,39 @@ Homepage: "${homepage}"
     required String path,
     String? output,
   }) async {
+    String basename = p.basename(path);
+    String path_script = p.join(Directory.current.path, "bin", "${basename}.dart");
+
     Directory directory = Directory(p.join(Directory.current.path, "build"));
+
     await directory.autoCreate();
     if (Platform.isLinux) {
       output ??= p.join(directory.path, "${p.basename(path)}-linux.deb");
       path = p.join(path, "linux", "packaging");
+      Process compile = await Process.start(
+        "dart",
+        [
+          "compile",
+          "exe",
+          path_script,
+          "-o",
+          p.join(path, "usr", "local", "bin", p.basename(basename)),
+        ],
+      );
+      await Process.start(
+        "dart",
+        [
+          "compile",
+          "exe",
+          path_script,
+          "-o",
+          p.join(path, "usr", "local", "share", p.basename(basename), p.basename(basename)),
+        ],
+      );
+      compile.stdout.listen((event) {
+        stdout.write(utf8.decode(event));
+      });
+      // stdout.addStream(compile.stdout);
       Process shell = await Process.start(
         "dpkg-deb",
         [
@@ -121,7 +165,11 @@ Homepage: "${homepage}"
           output,
         ],
       );
-      stdout.addStream(shell.stdout);
+      // stdout.addStream(shell.stdout);
+
+      shell.stdout.listen((event) {
+        stdout.write(utf8.decode(event));
+      });
       stderr.addStream(shell.stderr);
     }
     return;
