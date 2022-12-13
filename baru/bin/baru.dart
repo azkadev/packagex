4986f76a276ffcc1d878d4c223086b567b0fc9b2
￴ <>
2022-12-13 15:13:22 +0700

    update

---
+++ baru/bin/baru.dart
@@ -0,0 +1,5 @@
+import 'package:baru/baru.dart' as baru;
+
+void main(List<String> arguments) {
+  print('Hello world: ${baru.calculate()}!');
+}
---
+++ baru/test/baru_test.dart
@@ -0,0 +1,8 @@
+import 'package:baru/baru.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
